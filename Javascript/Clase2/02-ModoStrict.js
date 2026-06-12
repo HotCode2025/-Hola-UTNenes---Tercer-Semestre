c6837e06f5fc0a2419eb7ea815fedc66eac948eb
Serena Olmedo <serenadelvalle25@gmail.com>
2026-06-12 09:29:58 -0300

    Agrego la clase 2 de JavaScript

---
+++ Javascript/Clase2/02-ModoStrict.js
@@ -0,0 +1,14 @@
+//Video 1 y 2: Creamos un archivo de la clase de hoy y le ponemos de nombre: 02-ModoStrict.js
+"use strict";
+
+let x = 10;
+console.log(x);
+
+
+
+function miFuncion() {
+    "use strict";
+    let y = 10;
+    console.log(y);
+}
+miFuncion();
