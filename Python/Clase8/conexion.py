19849f6a70585606a6a28a3ea29cecd4429e7cd7
Otar Kebadze <OtarKebadze@gmail.com>
2026-06-22 16:05:35 -0300

    clase 8 python terminada

---
+++ Python/Clase8/conexion.py
@@ -0,0 +1,47 @@
+import psycopg2 as bd
+#psycopg2 as bd otra manera de importar el psycopg2
+from logger_base import log
+import sys
+
+
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _conexion = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        if cls._conexion is None:
+            try:
+                cls._conexion = bd.connect(host=cls._HOST,
+                                           user=cls._USERNAME,
+                                           password=cls._PASSWORD,
+                                           port=cls._DB_PORT,
+                                           database=cls._DATABASE)
+                log.debug(f'Conexion exitosa: {cls._conexion}')
+                return cls._conexion
+            except Exception as e:
+                # Usamos repr() para que convierta el error a texto puro y no rompa el UTF-8
+                log.error(f'Ocurrio un error: {repr(e)}')
+                sys.exit()
+        else:
+            return cls._conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+
+        try:
+            cursor = cls.obtenerConexion().cursor()
+            log.debug(f'Se abrió correctamente el cursor: {cursor}')
+            return cursor
+        except Exception as e:
+            log.error(f'Ocurrió un error: {e}')
+            sys.exit()
+
+
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
